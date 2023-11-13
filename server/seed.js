22977437c7b7705cfe20a380da2a387eb160bd30
Cordelia Snape <snapecordelia@gmail.com>
2023-11-13 13:13:20 +0000

    added more fake data

--- server/seed.js
+++ server/seed.js
@@ -7,37 +7,34 @@ mongoose.connect(process.env.DATABASE_URL);
 async function seed() {
   await Deal.create([
     {
-      businessName: "Pizza Hut",
-      offer: "50% off when you spend £25 or more",
-      discountDay: "Tuesday",
+      businessName: "Savor Delights",
+      offer: "Buy One Get One Free",
+      discountDay: "Wednesday",
       voucher: true,
-      location: "UK wide",
-      category: "Fast food",
-      businessImage:
-        "https://www.verdictfoodservice.com/wp-content/uploads/sites/17/2020/09/shutterstock_1150633349.jpg",
-      logoImage: "https://logowik.com/content/uploads/images/130_pizzahut.jpg",
+      location: "Liverpool",
+      category: "Cuisine Fusion",
+      businessImage: "https://loremflickr.com/600/600/restaurant",
+      logoImage: "https://loremflickr.com/400/400/logo,restaurant",
     },
     {
-      businessName: "Almost Famous",
-      offer: "Weekday £10 Burger & Fries 12 pm - 4 pm",
-      discountDay: "Weekday",
-      voucher: false,
-      location: "11-13 Parr Street, Liverpool, L1 4JN",
-      category: "Restaurant",
-      businessImage:
-        "https://www.tagvenue.com/resize/a8/f1/widen-1680-noupsize;15710-almost-famous-liverpool-venue.jpeg",
-      logoImage:
-        "https://static.wixstatic.com/media/08f7bc_a44ae6d1e1a94fbcb7db5bc30f964135~mv2.png/v1/crop/x_1,y_0,w_930,h_615/fill/w_418,h_277,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/08f7bc_a44ae6d1e1a94fbcb7db5bc30f964135~mv2.png",
+      businessName: "Tasty Bites",
+      offer: "Happy Hour Specials",
+      discountDay: "Wednesday",
+      voucher: true,
+      location: "Liverpool",
+      category: "Fine Dining",
+      businessImage: "https://loremflickr.com/600/600/restaurant,italian",
+      logoImage: "https://loremflickr.com/400/400/logo,restaurant",
     },
     {
-      businessName: "Maray",
-      offer: "2 dine for £40",
-      discountDay: "Weekday",
+      businessName: "Sizzle Grill",
+      offer: "Weekend Feast",
+      discountDay: "Saturday",
       voucher: false,
       location: "Liverpool",
-      category: "Middle Eastern Inspired Street Food",
-      businessImage:
-        "https://media-cdn.tripadvisor.com/media/photo-m/1280/1a/37/ec/c4/maray-is-a-fabulously.jpg",
+      category: "Grill & BBQ",
+      businessImage: "https://loremflickr.com/600/600/restaurant",
+      logoImage: "https://loremflickr.com/400/400/logo,restaurant",
     },
   ]);
 
