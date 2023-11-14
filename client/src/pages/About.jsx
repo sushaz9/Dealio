ae3fdaa0d8bf028317f2d808dfc87ec79d33f6ad
Cordelia Snape <snapecordelia@gmail.com>
2023-11-14 14:06:08 +0000

    added footer and about page

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <>
+      <h2>About the Developers</h2>
+      <div className="about-box" id="andy">
+        <h2>Andy</h2>
+        <p>Andy is a software developor on the Tech Educators course.</p>
+      </div>
+      <div className="about-box" id="susie">
+        <h2>Susie</h2>
+        <p>Susie is a software developor on the Tech Educators course.</p>
+      </div>
+      <div className="about-box" id="luke">
+        <h2>Luke</h2>
+        <p>Luke is a software developor on the Tech Educators course.</p>
+      </div>
+      <div className="about-box" id="cordelia">
+        <h2>Cordelia</h2>
+        <p>Cordelia is a software developor on the Tech Educators course.</p>
+      </div>
+    </>
+  );
+}
