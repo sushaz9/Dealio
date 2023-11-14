58efea87cedec651587f44b52fb6ae975e0c8aa1
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-11-14 10:17:17 +0000

    edit form.jsx

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,2 +1,27 @@
 import { useState } from "react";
 import axios from "axios";
+
+export default function Form({ deals, setDeals, deal, setDeal }) {
+  const [formData, setFormData] = useState(
+    book ?? {
+      businessName: "",
+      offer: "",
+      discountDay: "",
+      location: "",
+      category: "",
+      businessImage: "",
+      logoImage: "",
+    }
+  );
+}
+
+function handleChange(event) {
+  setFormData({ ...formData, [event.target.name]: event.target.value });
+}
+
+async function addDeal(event) {
+  event.preventDefault();
+  const API = "http://localhost:8080/";
+  const res = await axios.post(API, formData);
+  setBooks([...books, res.data]);
+}
